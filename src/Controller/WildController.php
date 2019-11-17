fa64ed149298904ab1fe65fb2c27122355c85e0a
Simon DUBOELLE <duboelle.s@gmail.com>
2019-11-17 13:24:19 +0100

    modification wildController

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -1,19 +1,19 @@
 <?php
-// src/Controller/DefaultController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-Class DefaultController extends AbstractController
+class WildController extends AbstractController
 {
     /**
-     * @Route("/", name="app_index")
+     * @Route("/wild", name="wild_index")
      */
-    public function index(): Response
+    public function index() :Response
     {
-        return $this->render('home.html.twig', [
+        return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
     }
