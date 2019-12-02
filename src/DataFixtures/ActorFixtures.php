4def2fecbc94df5dabb674ab865c592ccc9b1b32
Simon DUBOELLE <duboelle.s@gmail.com>
2019-12-03 00:09:50 +0100

    create ActorFixtures.php

---
+++ src/DataFixtures/ActorFixtures.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Actor;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+
+class ActorFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // TODO: Implement load() method.
+    }
+}
